13d73445b5d97791aaa50c6947683d85613c0712
DJ Matthews <knight1219@gmail.com>
2017-04-14 21:40:04 -0400

    Added more tests

--- CnotHttp.Portable/CnotHttp.IntegrationTests/GetTests.cs
+++ CnotHttp.Portable/CnotHttp.IntegrationTests/GetTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -21,7 +22,47 @@ namespace CnotHttp.IntegrationTests
             var test = await CnotHttp.Portable.CnotHttp.Get("https://jsonplaceholder.typicode.com/po/1");
             Assert.IsNotNull(test);
             Assert.IsNull(test.ServerResponse);
+            Assert.IsNotNull(test.Error);    
+        }
+
+        [TestMethod]
+        public async Task TestGetGoodObjectReturn()
+        {
+            var test = await CnotHttp.Portable.CnotHttp.Get<TestClasses.Post>("https://jsonplaceholder.typicode.com/posts/1");
+            Assert.IsNotNull(test);
+            Assert.IsNotNull(test.ServerResponse);
+            Assert.IsInstanceOfType(test.ServerResponse, typeof(TestClasses.Post));
+        }
+
+        [TestMethod]
+        public async Task TestGetGoodListOfObjectsReturn()
+        {
+            var test = await CnotHttp.Portable.CnotHttp.Get<List<TestClasses.Post>>("https://jsonplaceholder.typicode.com/posts");
+            Assert.IsNotNull(test);
+            Assert.IsNotNull(test.ServerResponse);
+            Assert.IsInstanceOfType(test.ServerResponse, typeof(List<TestClasses.Post>));
+        }
+
+
+        [TestMethod]
+        public async Task TestGetBadListOfObjectsReturn()
+        {
+            var test = await CnotHttp.Portable.CnotHttp.Get<List<TestClasses.Post>>("https://jsonplaceholder.typicode.com/posts/1");
+            Assert.IsNotNull(test);
+            Assert.IsNull(test.ServerResponse);
             Assert.IsNotNull(test.Error);
         }
+
+        [TestMethod]
+        public async Task TestGetBadErrorMessage()
+        {
+            var test = await CnotHttp.Portable.CnotHttp.Get<List<TestClasses.Post>>("https://jsonplaceholder.typicode.com/posts/1");
+            Assert.IsNotNull(test);
+            Assert.IsNull(test.ServerResponse);
+            Assert.IsNotNull(test.Error);
+            Assert.AreEqual(test.Error.Message, "Unable to convert response into object");
+        }
+
+        
     }
 }
--- CnotHttp.Portable/CnotHttp.IntegrationTests/TestClasses.cs
+++ CnotHttp.Portable/CnotHttp.IntegrationTests/TestClasses.cs
@@ -8,5 +8,22 @@ namespace CnotHttp.IntegrationTests
 {
     internal class TestClasses
     {
+
+        //{
+        //        "userId": 1,
+        //        "id": 1,
+        //        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        //        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        //}
+        public class Post
+        {
+            public int userId { get; set; }
+
+            public int id { get; set; }
+
+            public string title { get; set; }
+
+            public string body { get; set; }
+        }
     }
 }
